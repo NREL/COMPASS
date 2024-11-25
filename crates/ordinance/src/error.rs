2ba8d12c10c07ffa96d4d314c864fa77056c7aaa
Gui Castelao <guilherme@castelao.net>
2024-11-25 09:45:52 -0700

    feat: Implementing errors

--- crates/ordinance/src/error.rs
+++ crates/ordinance/src/error.rs
@@ -1,7 +1,16 @@
 /// Possible errors
 
 #[derive(Debug, thiserror::Error)]
-enum Error {
+pub(crate) enum Error {
     #[error(transparent)]
     IOError(#[from] std::io::Error),
+
+    #[allow(dead_code)]
+    #[error("Undefined error")]
+    // Used during development while it is not clear a category of error
+    // or when it is not worth to create a new error type.
+    /// Undefined error
+    Undefined(String),
 }
+
+pub(crate) type Result<T> = core::result::Result<T, Error>;
