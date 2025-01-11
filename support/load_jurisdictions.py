c85f3bff157db25b44e50383a756f45a85d1204c
Gui Castelao <guilherme@castelao.net>
2025-01-10 18:58:30 -0700

    support Python script to load ord_areas.gpkg

--- support/load_jurisdictions.py
+++ support/load_jurisdictions.py
@@ -1,14 +1,34 @@
 
+import duckdb
 import geopandas
 
 df = geopandas.read_file("ord_areas.gpkg")
+db = duckdb.connect("demo.db")
 
-states = df[["state_name", "geometry"]].dissolve(by="state_name")
+bookkeepping_id = db.sql("INSERT INTO bookkeeping (hash) values ('dummy_hash') RETURNING id").fetchone()[0]
 
-states.merge(df[["state_name", "state_fips"]].drop_duplicates(), on="state_name")
-county = df[["state_name", "state_fips", "county_name", "county_fips", "geometry"]].dropna()
-subd = df[["state_name", "state_fips", "subd_name", "subd_fips", "geometry"]].dropna()
+for _, per_state in df.groupby(["state_fips"]):
+    state = per_state[["state_name", "geometry"]].dissolve(by="state_name")
+    state = state.merge(per_state[["state_name", "state_fips"]].drop_duplicates(), on="state_name").iloc[0]
 
+    state_pk = db.execute(f"INSERT INTO jurisdiction (name, bookkeeping_lnk, fips, rank, geometry) values ('{state.state_name}', {bookkeepping_id}, {state.state_fips}, 'state', '{state.geometry}') RETURNING id").fetchone()[0]
 
-insert into bookkeeping (hash) values ('dummy_hash');
-INSERT INTO jurisdiction (name, bookkeeping_lnk, fips, rank, geometry) VALUES ('Kentucky', 1, 7, 'county', 'POLYGON ((-89.181 37.046, -88.815 36.954, -89.053 36.932, -89.181 37.046))');
+    county = per_state[["state_name", "state_fips", "county_name", "county_fips", "geometry"]].dropna()
+    for _, site in county.iterrows():
+        db.execute(f"INSERT INTO jurisdiction (bookkeeping_lnk, parent_id, name, FIPS, rank, geometry) values ({bookkeepping_id}, {state_pk}, '{site.county_name.replace("'", "''''")}', {site.county_fips}, 'county', '{site.geometry}')")
+
+    subd = per_state[["state_name", "state_fips", "subd_name", "subd_fips", "geometry"]].dropna()
+    for _, site in subd.iterrows():
+        if site.subd_name.endswith('town'):
+            rank = "town"
+        elif site.subd_name.endswith('city'):
+            rank = "city"
+        elif site.subd_name.endswith('CCD'):
+            rank = "CCD"
+        elif site.subd_name.endswith('Reservation'):
+            rank = "reservation"
+        else:
+            rank = "other"
+            print(site.subd_name)
+        db.execute(f"INSERT INTO jurisdiction (bookkeeping_lnk, parent_id, name, FIPS, rank, geometry) values ({bookkeepping_id}, {state_pk}, '{site.subd_name.replace("'", "''''")}', {site.subd_fips}, '{rank}', '{site.geometry}')")
+
