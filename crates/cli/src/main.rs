27c814d2cd835621158a0070634fec7864716b73
Gui Castelao <guilherme@castelao.net>
2025-01-15 17:30:03 -0700

    Reading the path for subcommand load

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -57,8 +57,10 @@ fn main() {
             ordinance::export_db(&db);
         }
         Some("load") => {
-            let path = matches.get_one::<PathBuf>("path").expect("required");
-            trace!("Loading data from {:?} into database at {:?}", &path, &db);
+            trace!("Subcommand load");
+            let path = matches.subcommand_matches("load").unwrap().get_one::<PathBuf>("path").unwrap();
+            trace!("Loading data from: {:?}, into database: {:?}", &path, &db);
+            dbg!(&path);
             ordinance::scan_features(&db, path);
         }
         Some("log") => {
