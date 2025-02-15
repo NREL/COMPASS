845e0f878daf82141a054afe882dab8c5da3f0af
ppinchuk <pinchukpaul@gmail.com>
2025-02-15 09:01:36 -0700

    move `possibly_mentions_wind` function

--- compass/extraction/wind/__init__.py
+++ compass/extraction/wind/__init__.py
@@ -1,4 +1,5 @@
 """Wind ordinance extraction utilities"""
 
+from .document_validation import possibly_mentions_wind
 from .ordinance import WindOrdinanceTextExtractor, WindOrdinanceValidator
 from .parse import StructuredWindOrdinanceParser
