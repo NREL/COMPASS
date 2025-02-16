e176e3e4bea0d06fc3b2a9913b9b33b58c874b5e
ppinchuk <pinchukpaul@gmail.com>
2025-02-15 20:34:56 -0700

    Update tests

--- tests/python/extraction/test_extraction_validation.py
+++ tests/python/extraction/test_extraction_validation.py
@@ -1,10 +1,11 @@
-"""COMPASS wind ordinance validation tests"""
+"""COMPASS ordinance validation tests"""
 
 from pathlib import Path
 
 import pytest
 
-from compass.extraction.wind import possibly_mentions_wind
+from compass.extraction.wind.ordinance import WindHeuristic
+from compass.extraction.solar.ordinance import SolarHeuristic
 
 
 @pytest.mark.parametrize(
@@ -22,9 +23,29 @@ from compass.extraction.wind import possibly_mentions_wind
     ],
 )
 def test_possibly_mentions_wind(text, truth):
-    """Test for `possibly_mentions_wind` function (basic execution)"""
+    """Test for `WindHeuristic` class (basic execution)"""
 
-    assert possibly_mentions_wind(text) == truth
+    assert WindHeuristic().check(text) == truth
+
+
+@pytest.mark.parametrize(
+    "text,truth",
+    [
+        ("Solar SETBACKS", True),
+        (" SECS SETBACKS", True),
+        ("SOLARIS SETBACKS", False),
+        ("WECS SETBACKS", False),
+        ("(solar farm)", True),
+        ("Solar WECS", False),
+        ("Solar SES", False),
+        ("Solar SES\n", True),
+        ("solar panels and solar farms", True),
+    ],
+)
+def test_possibly_mentions_solar(text, truth):
+    """Test for `SolarHeuristic` class (basic execution)"""
+
+    assert SolarHeuristic().check(text) == truth
 
 
 if __name__ == "__main__":
