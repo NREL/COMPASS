b69e3cbeb0b5aecd3c99a5dcaf05498ccc87c294
ppinchuk <pinchukpaul@gmail.com>
2025-04-09 16:51:56 -0600

    Remove broken init

--- compass/llm/__init__.py
+++ compass/llm/__init__.py
@@ -1,24 +1,8 @@
 """COMPASS Ordinance LLM callers"""
 
-from enum import StrEnum, auto
-
 from .calling import (
     LLMCaller,
     ChatLLMCaller,
     StructuredLLMCaller,
     LLMCallerArgs,
 )
-
-
-class LLMUsageCategory(StrEnum):
-    """COMPASS LLM usage categories"""
-
-    CHAT = auto()
-    DATE_EXTRACTION = auto()
-    DECISION_TREE = auto()
-    DEFAULT = auto()
-    DOCUMENT_CONTENT_VALIDATION = auto()
-    DOCUMENT_ORDINANCE_SUMMARY = auto()
-    DOCUMENT_PERMITTED_USE_CONTENT_VALIDATION = auto()
-    DOCUMENT_PERMITTED_USE_DISTRICTS_SUMMARY = auto()
-    DOCUMENT_LOCATION_VALIDATION = auto()
