5f32ff2c852fb094f7930f1d72aaccb75410a324
ppinchuk <pinchukpaul@gmail.com>
2025-04-09 18:00:49 -0600

    Add descriptions

--- compass/utilities/enums.py
+++ compass/utilities/enums.py
@@ -21,12 +21,50 @@ class LLMTasks(StrEnum):
     """LLM-based COMPASS tasks"""
 
     DATE_EXTRACTION = LLMUsageCategory.DATE_EXTRACTION
+    """Date extraction task"""
+
     DEFAULT = LLMUsageCategory.DEFAULT
+    """Default fallback option for all tasks"""
+
     DOCUMENT_CONTENT_VALIDATION = LLMUsageCategory.DOCUMENT_CONTENT_VALIDATION
+    """Document content validation task
+
+    This represents a task like "does the document contain ordinance
+    values" or "does the document contain permitted use specifications".
+    """
+
     DOCUMENT_LOCATION_VALIDATION = (
         LLMUsageCategory.DOCUMENT_LOCATION_VALIDATION
     )
+    """Document location validation task
+
+    This represents all the tasks associated with validation that the
+    document pertains to a particular jurisdiction.
+    """
+
     ORDINANCE_TEXT_EXTRACTION = auto()
+    """Ordinance text extraction task
+
+    This task represents the extraction/summarization of text containing
+    ordinance values.
+    """
+
     PERMITTED_USE_TEXT_EXTRACTION = auto()
+    """Permitted use text extraction task
+
+    This task represents the extraction/summarization of text containing
+    permitted use descriptions and allowances.
+    """
+
     ORDINANCE_VALUE_EXTRACTION = auto()
+    """Ordinance structured value extraction task
+
+    This task represents the extraction of structured ordinance values.
+    """
+
     PERMITTED_USE_VALUE_EXTRACTION = auto()
+    """Permitted use structured value extraction task
+
+    This task represents the extraction of structured permitted use
+    values.
+    """
