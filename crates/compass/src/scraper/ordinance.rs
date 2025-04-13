87e2879a90d31b00d63827eaff09df687f9ff776
Gui Castelao <guilherme@castelao.net>
2025-04-12 19:46:02 -0600

    feat: Ordinance::init_db

--- crates/compass/src/scraper/ordinance.rs
+++ crates/compass/src/scraper/ordinance.rs
@@ -1,5 +1,8 @@
 //! Parse and handle the scrapped ordinance information
 
+use tracing::trace;
+
+use crate::error::Result;
 
 #[allow(non_snake_case)]
 #[derive(Debug, serde::Deserialize)]
@@ -20,3 +23,38 @@ pub(super) struct Ordinance {
     section: Option<String>,
     source: Option<String>,
 }
+
+impl Ordinance {
+    pub(super) fn init_db(conn: &duckdb::Transaction) -> Result<()> {
+        trace!("Initializing database for Ordinance");
+
+        conn.execute_batch(
+            r"
+            CREATE SEQUENCE IF NOT EXISTS ordinance_sequence START 1;
+            CREATE TABLE IF NOT EXISTS ordinance (
+              id INTEGER PRIMARY KEY DEFAULT
+                NEXTVAL('ordinance_sequence'),
+              bookkeeper_lnk INTEGER REFERENCES bookkeeper(id) NOT NULL,
+              county TEXT,
+              state TEXT,
+              subdivison TEXT,
+              jurisdiction_type TEXT,
+              FIPS INTEGER,
+              feature TEXT,
+              value REAL,
+              units TEXT,
+              offset REAL,
+              min_dist REAL,
+              max_dist REAL,
+              summary TEXT,
+              ord_year INTEGER,
+              section TEXT,
+              source TEXT
+            );",
+        )?;
+
+        trace!("Database ready for Ordinance");
+        Ok(())
+    }
+}
+
