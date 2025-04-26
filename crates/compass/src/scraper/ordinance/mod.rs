e3598220fbdc29a6b28553ccc8c66fad0b5a19a0
Gui Castelao <guilherme@castelao.net>
2025-04-26 09:44:03 -0600

    refact: Ordinance is now composed by [quali + quanti]tative

--- crates/compass/src/scraper/ordinance/mod.rs
+++ crates/compass/src/scraper/ordinance/mod.rs
@@ -8,56 +8,17 @@ use tracing::trace;
 use crate::error::Result;
 
 #[derive(Debug)]
-pub(super) struct Ordinance(Vec<OrdinanceRecord>);
-
-#[allow(dead_code, non_snake_case)]
-#[derive(Debug, serde::Deserialize)]
-pub(super) struct OrdinanceRecord {
-    county: String,
-    state: String,
-    subdivison: Option<String>,
-    jurisdiction_type: Option<String>,
-    FIPS: u32,
-    feature: String,
-    value: f64,
-    units: Option<String>,
-    offset: Option<f64>,
-    min_dist: Option<f64>,
-    max_dist: Option<f64>,
-    summary: Option<String>,
-    ord_year: Option<u32>,
-    section: Option<String>,
-    source: Option<String>,
+pub(super) struct Ordinance{
+    quantiative: quantitative::Quantitative,
+    qualitative: qualitative::Qualitative,
 }
 
 impl Ordinance {
     pub(super) fn init_db(conn: &duckdb::Transaction) -> Result<()> {
         trace!("Initializing database for Ordinance");
 
-        conn.execute_batch(
-            r"
-            CREATE SEQUENCE IF NOT EXISTS ordinance_sequence START 1;
-            CREATE TABLE IF NOT EXISTS ordinance (
-              id INTEGER PRIMARY KEY DEFAULT
-                NEXTVAL('ordinance_sequence'),
-              bookkeeper_lnk INTEGER REFERENCES bookkeeper(id) NOT NULL,
-              county TEXT,
-              state TEXT,
-              subdivison TEXT,
-              jurisdiction_type TEXT,
-              FIPS INTEGER,
-              feature TEXT,
-              value REAL,
-              units TEXT,
-              adder REAL,
-              min_dist REAL,
-              max_dist REAL,
-              summary TEXT,
-              ord_year INTEGER,
-              section TEXT,
-              source TEXT
-            );",
-        )?;
+        quantitative::Quantitative::init_db(conn)?;
+        qualitative::Qualitative::init_db(conn)?;
 
         trace!("Database ready for Ordinance");
         Ok(())
@@ -67,83 +28,21 @@ impl Ordinance {
     pub(super) async fn open<P: AsRef<std::path::Path>>(root: P) -> Result<Ordinance> {
         trace!("Opening quantitative ordinance of {:?}", root.as_ref());
 
-        let path = root.as_ref().join("quantitative_ordinances.csv");
-        if !path.exists() {
-            trace!("Missing quantitative ordinance file: {:?}", path);
-            return Err(crate::error::Error::Undefined(
-                "Missing quantitative ordinance file".to_string(),
-            ));
-        }
+        let ordinance = Ordinance{
+            quantiative: quantitative::Quantitative::open(root.as_ref()).await?,
+            qualitative: qualitative::Qualitative::open(root.as_ref()).await?,
+        };
 
-        trace!("Identified quantitative ordinance at {:?}", path);
+        trace!("Opened ordinance: {:?}", ordinance);
 
-        /*
-        let df = CsvReadOptions::default()
-            .with_has_header(true)
-            .try_into_reader_with_file_path(path.into()).unwrap()
-            .finish()
-            .unwrap();
-        */
-
-        let mut rdr = csv::ReaderBuilder::new()
-            .has_headers(true)
-            .delimiter(b',')
-            .from_path(&path)
-            .unwrap();
-
-        trace!("Quantitative reader {:?}", rdr);
-
-        let mut output = Vec::new();
-        for result in rdr.deserialize() {
-            let record: OrdinanceRecord = match result {
-                Ok(record) => record,
-                Err(_) => {
-                    trace!("Error {:?}", result);
-                    continue;
-                }
-            };
-            output.push(record);
-        }
-        trace!("Quantitative ordinance records {:?}", output);
-
-        Ok(Ordinance(output))
+        Ok(ordinance)
     }
 
     pub(super) fn write(&self, conn: &duckdb::Transaction, commit_id: usize) -> Result<()> {
         trace!("Writing ordinance to database");
 
-        let mut stmt = conn
-            .prepare(
-                r"INSERT INTO ordinance
-            (bookkeeper_lnk, county, state, subdivison,
-            jurisdiction_type, FIPS, feature, value, units, adder,
-            min_dist, max_dist, summary, ord_year, section, source)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            ",
-            )
-            .expect("Failed to prepare ordinance statement");
-
-        for record in &self.0 {
-            trace!("Writing ordinance record {:?}", &record);
-            stmt.execute(duckdb::params![
-                commit_id,
-                record.county,
-                record.state,
-                record.subdivison,
-                record.jurisdiction_type,
-                record.FIPS,
-                record.feature,
-                record.value,
-                record.units,
-                record.offset,
-                record.min_dist,
-                record.max_dist,
-                record.summary,
-                record.ord_year,
-                record.section,
-                record.source,
-            ])?;
-        }
+        self.quantiative.write(conn, commit_id)?;
+        self.qualitative.write(conn, commit_id)?;
 
         trace!("Ordinance written to database");
         Ok(())
