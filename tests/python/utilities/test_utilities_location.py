3433f65e63fcf611e5cca7899a5e129bae0cd618
ppinchuk <pinchukpaul@gmail.com>
2025-05-19 20:09:44 -0600

    Add another test

--- tests/python/utilities/test_utilities_location.py
+++ tests/python/utilities/test_utilities_location.py
@@ -8,7 +8,7 @@ from compass.utilities.location import Jurisdiction
 
 
 def test_basic_state_properties():
-    """Tets basic properties for ``Jurisdiction`` class for a state"""
+    """Test basic properties for ``Jurisdiction`` class for a state"""
 
     state = Jurisdiction("state", state="Colorado")
 
@@ -24,7 +24,7 @@ def test_basic_state_properties():
 
 
 def test_basic_county_properties():
-    """Tets basic properties for ``Jurisdiction`` class for a county"""
+    """Test basic properties for ``Jurisdiction`` class for a county"""
 
     county = Jurisdiction("county", county="Box Elder", state="Utah")
 
@@ -41,7 +41,7 @@ def test_basic_county_properties():
 
 @pytest.mark.parametrize("jt", ["town", "city", "borough"])
 def test_basic_town_properties(jt):
-    """Tets basic properties for ``Jurisdiction`` class for a town"""
+    """Test basic properties for ``Jurisdiction`` class for a town"""
 
     town = Jurisdiction(
         jt, county="Jefferson", state="Colorado", subdivision_name="Golden"
@@ -68,7 +68,7 @@ def test_basic_town_properties(jt):
 
 
 def test_atypical_subdivision_properties():
-    """Tets basic properties for ``Jurisdiction`` class for a subdivision"""
+    """Test basic properties for ``Jurisdiction`` class for a subdivision"""
 
     gore = Jurisdiction(
         "gore", county="Chittenden", state="Vermont", subdivision_name="Buels"
@@ -90,6 +90,26 @@ def test_atypical_subdivision_properties():
 
     assert gore == "Buels Gore, Chittenden County, Vermont"
     assert gore == "buels gOre, chittENden county, vermonT"
+
+
+def test_city_no_county():
+    """Test ``Jurisdiction`` for a city with no county"""
+
+    gore = Jurisdiction("city", "Maryland", subdivision_name="Baltimore")
+
+    assert repr(gore) == "City of Baltimore, Maryland"
+    assert gore.full_name == "City of Baltimore, Maryland"
+    assert gore.full_name == str(gore)
+
+    assert gore == Jurisdiction(
+        "city", "maryland", subdivision_name="baltimore"
+    )
+    assert gore != Jurisdiction(
+        "county", "maryland", subdivision_name="baltimore"
+    )
+
+    assert gore == "City of Baltimore, Maryland"
+    assert gore == "ciTy of baltiMore, maryland"
 
 
 if __name__ == "__main__":
