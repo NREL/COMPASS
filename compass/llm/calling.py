6a1270fa5d74be3d0570f38c85ed72f06d7e1ab0
ppinchuk <pinchukpaul@gmail.com>
2025-06-21 16:39:50 -0600

    Minor docstring update

--- compass/llm/calling.py
+++ compass/llm/calling.py
@@ -113,7 +113,6 @@ class ChatLLMCaller(BaseLLMCaller):
             processing function (i.e. `llm_service.call(**kwargs)`).
             Should *not* contain the following keys:
 
-                - usage_tracker
                 - usage_sub_label
                 - messages
 
