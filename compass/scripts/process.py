1c2c20793f2dbc4b63f5fa8416acde798b405215
ppinchuk <pinchukpaul@gmail.com>
2025-08-07 10:49:38 -0600

    Fix docstring

--- compass/scripts/process.py
+++ compass/scripts/process.py
@@ -256,13 +256,13 @@ async def process_jurisdictions_with_openai(  # noqa: PLR0917, PLR0913
         at `www.co.delaware.in.us/documents/1649699794_0382.pdf`.
         By default, ``None``.
     known_doc_urls : dict | str, optional
-        A dictionary where keys are the jurisdiction codes and values
-        are a string or list of strings representing known URL's to
-        check for those jurisdictions. If provided, these URL's will be
-        checked first and if an ordinance document is found, no further
-        scraping will be performed. This inout can also be a path to a
-        JSON file containing the dictionary of code-to-url mappings.
-        By default, ``None``.
+        A dictionary where keys are the jurisdiction codes (as strings)
+        and values are a string or list of strings representing known
+        URL's to check for those jurisdictions. If provided, these URL's
+        will be checked first and if an ordinance document is found, no
+        further scraping will be performed. This input can also be a
+        path to a JSON file containing the dictionary of code-to-url
+        mappings. By default, ``None``.
     file_loader_kwargs : dict, optional
         Dictionary of keyword arguments pairs to initialize
         :class:`elm.web.file_loader.AsyncFileLoader`. If found, the
